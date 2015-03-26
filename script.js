70816333253a358a0a8f8752131532cad9c6f723
cSherman42 <csherman360@gmail.com>
2015-03-26 13:00:47 -0500

    Implementing Shrek into website
    Allahu Akbar, shrek is now a link on my new main website, old website is
    RIP, idk why but i dont want it anymore :/

---
+++ script.js
@@ -0,0 +1,13 @@
+function getLastDate(){
+    var last = new Date(document.lastModified);
+    var result = (last.getMonth()+1).toString() + "/"+ last.getDate().toString() + "/" + last.getFullYear().toString();
+    return result;
+}
+
+//main
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById("dateModified").innerHTML = getLastDate();
+    console.log(getLastDate());
+    
+});
