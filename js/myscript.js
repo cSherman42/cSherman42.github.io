c1b378852ba789c50c1d59dcf7bf093e15f5c994
cSherman42 <csherman360@gmail.com>
2015-04-14 09:54:08 -0500

    Brought Shrek back to life
    Hello Shrek

--- js/myscript.js
+++ js/myscript.js
@@ -2,18 +2,18 @@ var image = null;
 window.onload = function() {
         console.log("function initialized");
         image = document.getElementById("meme");
-        image.src = "http://ts1.mm.bing.net/th?        &id=HN.608049129170994523&w=300&h=300&c=0&pid=1.9&rs=0&p=0&url=http%3A%2F%2Fwww.rlsgame.org%2Fpc%2F1-pc%2F1986-shrek-forever-after-vitality.html";
+        image.src = "http://ts1.mm.bing.net/th?&id=HN.608049129194456409&w=300&h=300&c=0&pid=1.9&rs=0&p=0&url=http%3A%2F%2Fwww.rlsgame.org%2Fpc%2F1-pc%2F1986-shrek-forever-after-vitality.html";
         
         
     
 }
 
 function myFunk(){
-if(image.src.match("forever")) {
+if(image.src.match("rlsgame")) {
             console.log("regex works");
-            image.src = "http://ts1.mm.bing.net/th?id=HN.608037279357207284&pid=15.1";
+            image.src = "http://ts1.mm.bing.net/th?&id=HN.607992813582878622&w=300&h=300&c=0&pid=1.9&rs=0&p=0";
         }
         else {
-            image.src = "http://ts1.mm.bing.net/th?        &id=HN.608049129170994523&w=300&h=300&c=0&pid=1.9&rs=0&p=0&url=http%3A%2F%2Fwww.rlsgame.org%2Fpc%2F1-pc%2F1986-shrek-forever-after-vitality.html";
+            image.src = "http://ts1.mm.bing.net/th?&id=HN.608049129194456409&w=300&h=300&c=0&pid=1.9&rs=0&p=0&url=http%3A%2F%2Fwww.rlsgame.org%2Fpc%2F1-pc%2F1986-shrek-forever-after-vitality.html";
         }
 }
